ea99dda98ab53969143feaf8f39fbe879995bfa5
Celine Bowen <65847718+celine-debug@users.noreply.github.com>
2020-07-13 17:46:44 +0300

    Add files via upload

---
+++ nav.php
@@ -0,0 +1,19 @@
+
+<header>
+		<div class="container">
+			<div id="branding">
+				<h1><span class highlight>CUK Tech club</span></h1>
+			</div>
+			<nav>
+				<ul>
+					<li class="current"><a href="index.php">Home</li>
+						<li><a href="about.php">About</li>
+				           <li><a href="log.php">Login</li>
+				           	<li><a href="achievements.php">Achievements</li>
+				           		<li><a href="events.php">Events</li>
+
+
+				</ul>
+			</nav>
+		</div>
+	</header>
